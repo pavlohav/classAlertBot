799082a01344924850031a3d63ca7c3f00f06451
Pavlo Havrylyuk <12107961+pavlohav@users.noreply.github.com>
2024-11-26 01:47:49 +0000

    Remove auth

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 var logger = require('winston');
-var auth = require('./auth.json');
 var axios = require('axios')
 var fs = require('fs');
 
